8b56645361c9444b8ff1217b3a5dc4d48857935f
d2jackso <d2jackso@gmail.com>
2014-07-21 15:50:22 -0600

    update title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Twister App POC"
+    if page_title.is_empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
